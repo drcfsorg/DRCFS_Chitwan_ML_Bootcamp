965699fb4187f3888f3cdc602807b447135771a3
kafka <bishalkharel0000@gmail.com>
2023-04-26 17:23:42 +0545

    delete: unnecessary files

--- Day 7/index.py
+++ Day 7/index.py
@@ -1,1 +1,26 @@
+
+
+
+
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+
+
+app=FastAPI()
+
+with open("randomfile.txt", 'r') as f:
+    content=f.read()
+
+print(content)
+
+@app.get("/")
+async def homepage():
+    return HTMLResponse("Hello guys this is homepage!!")
+
+
+@app.get("/testing")
+async def randomtesting():
+    return HTMLResponse(content)
+ 
+
